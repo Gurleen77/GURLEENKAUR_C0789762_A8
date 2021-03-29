45f5d67030ad2a18c61eef34dc9e0603dfcb9ef6
Navdeep92 <balgurleen77@gmail.com>
2021-03-29 14:34:22 -0400

    Changes of comment

--- src/GURLEENKAUR_C0789762_A8/Temperature.java
+++ src/GURLEENKAUR_C0789762_A8/Temperature.java
@@ -29,6 +29,7 @@ public class Temperature {
     }
 
     //HERE ARE THE COMMENTS
+
     public double getTemperature(String day){
         double temp = 0;
         temp = Double.parseDouble(JOptionPane.showInputDialog("Please enter the temperature of the " + day));
